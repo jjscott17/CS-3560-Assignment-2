4bd0953826dbcc2259c03d36941c049aa3474e63
Joseph Scott <165531069+jjscott17@users.noreply.github.com>
2025-04-15 23:49:00 -0700

    Update Main.java

--- oop-assignment-2-jjscott17/src/main/java/org/example/Main.java
+++ oop-assignment-2-jjscott17/src/main/java/org/example/Main.java
@@ -1,6 +1,30 @@
 package org.example;
 
 public class Main {
-
-
+    public static void main(String[] args) {
+        PyramidRubik cube = new PyramidRubik();
+        cube.display();
+        System.out.println("\nRotating...");
+        System.out.println("Left clockwise:");
+        cube.rotateLeftClockwise(0);
+        cube.display();
+        System.out.println("Right clockwise:");
+        cube.rotateRightClockwise(1);
+        cube.display();
+        System.out.println("Left Counter clockwise");
+        cube.rotateLeftCounterClockwise(2);
+        cube.display();
+        System.out.println("Right Counter clockwise");
+        cube.rotateRightCounterClockwise(3);
+        cube.display();
+        System.out.println("Top clockwise");
+        cube.rotateTopClockwise(0);
+        cube.display();
+        System.out.println("Top Counter clockwise");
+        cube.rotateTopCounterClockwise(0);
+        cube.display();
+        System.out.println("\nScrambling:");
+        cube.scramble();
+        cube.display();
+    }
 }
